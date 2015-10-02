c2ad503c7d5cfa42f2b2c02f1dcf8a7fb1b33853
Adailson <adailsonfilho@gmail.com>
2015-10-02 00:27:26 -0300

    correções de contagem de classes no preprocessamento e arquivo de dados

--- Questao2/Bayes.py
+++ Questao2/Bayes.py
@@ -17,41 +17,64 @@ class ClassEnum(Enum): #how use that shit? Class['positive']
 
 class Bayes():
 	def __init__(self):
+		self.totalPositive = 0
+		self.totalNegative = 0
 		self.data = []
 
 	def preProcess(self, line, separator):
 		#Adailson: Este método poderia ser feito com split(','), mas a performance ia cair um pouco (onde a gente puder ganhar em performance nesse tipo de aplicação é importante)
 
-		features = []#ira armazenas os dados convertidos. Ex: "x,x,x,o,o,o,b,b,b" vira [1,1,1,0,0,0,-1,-1,-1] (de acordo com a classe enum)
+		features = []#ira armazenas os dados convertidos. Ex: "x,x,x,o,o,o,b,b,b,positivo" vira ([1,1,1,0,0,0,-1,-1,-1],1) (de acordo com a classe enum)
 		classId = None
 		acc = '' #acumulador
 
-		# line += ','#para servir de referencia do fim do token
+		#line += '$'#para servir de referencia do fim do token
 
 		print(line)
 		for x in line:
-			if x != ',':
+			if x != ',' and x != '\n':
 				acc += x
 			elif x == '\n':
-				classId = ClassEnum[acc]
+				classId = ClassEnum[acc].value
+				if acc == ClassEnum.negative.name:
+					self.totalNegative +=1
+				elif acc == ClassEnum.positive.name:
+					self.totalPositive +=1
 			else:
-				features.append(FeatureEnum[acc]) # salva a categoria comseu identificador de acordo com a classe enum
+				features.append(FeatureEnum[acc].value) # salva a categoria comseu identificador de acordo com a classe enum
 				acc = ''#reseta
-		
-
+		print(str(features)+" "+str(classId))
 		return (features,classId)
 
 	def readData(self, path):
 		with open(path) as f:
 			for line in f:
-				processedLine = self.preProcess(line,',')
+				processedLine = self.preProcess(line,separator=',')
 				self.data.append(processedLine)
 
 
 	def priori(self, classId):
+		if classId == ClassEnum.negative:
+			return self.totalNegative/(self.totalNegative+self.totalPositive)
+		elif classId == ClassEnum.positive:
+			return self.totalPositive/(self.totalNegative+self.totalPositive)
+
+	def posteriori(self, classId, features):
 		pass
 
-	def posteriori(self,classId,features):
+	def conditionalDensity(self, features, classId):
+		pass
+
+	#probabilidade condicional  pij = P(xi = 1|ωj ) -> 'x'
+	def pij(xi, classId):
+		pass
+
+	#probabilidade condicional  qij = P(xi = 0|ωj ) -> 'o'
+	def qij(xi, classId):
+		pass
+
+	#probabilidade condicional  qij = P(xi = -1|ωj ) -> 'b'
+	def rij(xi, classId):
 		pass
 
 	def runForestRun(self):
@@ -61,3 +84,6 @@ if __name__ == "__main__":
 
 	bayes = Bayes() #Cria instancia da casse Bayes
 	bayes.runForestRun()
+	print(bayes.totalPositive)
+	print(bayes.totalNegative)
+	print(str(bayes.totalNegative+bayes.totalPositive))
